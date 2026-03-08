415358c918c12ef7bd5ba19fb43c440be6e29db3
Ceo-Sammarco <noah.kd@gmail.com>
2026-03-07 23:38:07 -0300

    update:  M components/layout/AppBar.tsx

--- components/layout/AppBar.tsx
+++ components/layout/AppBar.tsx
@@ -66,7 +66,7 @@ export default function AppBar({
     }
   };
 
-  const styled = !transparent
+  const styled: React.CSSProperties = !transparent
     ? {
         background: bg,
         borderBottom: borderb,
