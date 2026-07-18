c8e346ffcea1d21434251cccef6d04f5d296f540
Ceo-Sammarco <noah.kd@gmail.com>
2026-07-18 20:32:45 -0300

    update:  D docs/02.sql  D docs/sql/01.sql  D docs/sql/03.sql

--- docs/sql/03.sql
+++
@@ -1,9 +0,0 @@
--- adiciona as colunas para cliente não cadastrado em db no orçamento
-
-ALTER TABLE orcamentos 
-ADD COLUMN zip TEXT,
-ADD COLUMN street TEXT,
-ADD COLUMN number TEXT,
-ADD COLUMN complement TEXT,
-ADD COLUMN neighborhood TEXT,
-ADD COLUMN city TEXT;
