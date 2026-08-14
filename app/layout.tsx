610dc30c81395838614affed69c8a66dbd9266c8
Ceo-Sammarco <noah.kd@gmail.com>
2026-08-14 01:13:45 -0300

    update:  M app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,7 @@ const godOfThunder = localFont({
 });
 
 export const viewport: Viewport = {
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#f8fafc' },
-    { media: '(prefers-color-scheme: dark)', color: '#0f172a' },
-  ],
+  themeColor: '#00559c',
 };
 
 export const metadata: Metadata = {
